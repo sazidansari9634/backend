2d84fc66344d8676372a4346e432b10721366d3a
Sazid Ansari <sazidansari9634@gmail.com>
2023-03-16 16:24:35 +0545

    api integration with index all data and show data by id api is completed

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,8 +1,18 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
+    articles = Article.all
+    render json: articles, status: 200
   end
 
   def show
+    article = Article.find_by(id: params[:id])
+    if article
+      render json: article, status: 200
+    else
+      render json: {
+        error: "Article not found"
+      }
+    end
   end
 
   def create
